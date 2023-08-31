12c9d9ed713d2e4ac669f34c224f5acf87953ac9
lmlenis <llenis73@gmail.com>
2023-08-31 10:44:20 -0500

    integrador 31ago2023

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,14 @@
+import me from "../assets/me2.jpg"
+
 const About = () => {
     return(
         <div>
-            <h1>Work in progress</h1>
+            <h2>LuisMa</h2>
+            <h2>Humanoid</h2>
+            <h2>Male</h2>
+            <h2>Alive</h2>
+            <h2>Colombia</h2>
+            <img src= {me} alt='' />
         </div>
 
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,10 +7,10 @@ const Nav = ({ onSearch, randomize, logout }) => {
         <nav>
             <SearchBar onSearch = {onSearch}/>
             <button onClick = {randomize}>ADD RANDOM</button>
-            <button onClick = {logout}>Log Out</button>
+            <button onClick = {logout}>LOG OUT</button>
             <button><Link to='/about'>ABOUT</Link></button>
             <button><Link to='/home' >HOME</Link></button>
-            <button><Link to='/favorites' >Favorites</Link></button>
+            <button><Link to='/favorites' >FAVORITES</Link></button>
         </nav>
     )
 }
